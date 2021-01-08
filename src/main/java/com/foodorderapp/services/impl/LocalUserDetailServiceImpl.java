c5da39779a2bc98caa712d927d8c04e2d1707cd2
VladAngelov <v.angelov1919@gmail.com>
2021-01-09 00:33:50 +0200

    Fixed user loading.

--- src/main/java/com/foodorderapp/services/impl/LocalUserDetailServiceImpl.java
+++ src/main/java/com/foodorderapp/services/impl/LocalUserDetailServiceImpl.java
@@ -2,8 +2,11 @@ package com.foodorderapp.services.impl;
 
 import com.foodorderapp.exceptions.ResourceNotFoundException;
 import com.foodorderapp.models.binding.user.LocalUserBindingModel;
+import com.foodorderapp.models.entity.Role;
 import com.foodorderapp.models.entity.User;
 import com.foodorderapp.models.service.UserServiceModel;
+import com.foodorderapp.repositories.RoleRepository;
+import com.foodorderapp.repositories.UserRepository;
 import com.foodorderapp.services.interfaces.UserService;
 import com.foodorderapp.util.GeneralUtils;
 import org.modelmapper.ModelMapper;
@@ -13,16 +16,22 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class LocalUserDetailServiceImpl implements UserDetailsService {
 
     private final UserService userService;
     private final ModelMapper modelMapper;
+    private final UserRepository userRepository;
 
     @Autowired
     public LocalUserDetailServiceImpl(
+            UserRepository userRepository,
             UserService userService,
             ModelMapper modelMapper) {
+        this.userRepository = userRepository;
         this.userService = userService;
         this.modelMapper = modelMapper;
     }
@@ -48,13 +57,15 @@ public class LocalUserDetailServiceImpl implements UserDetailsService {
     }
 
     private LocalUserBindingModel createLocalUser(User user) {
+       var u = this.userRepository.findByEmail(user.getEmail());
+
         return new LocalUserBindingModel(
-                user.getEmail(),
-                user.getPassword(),
-                user.isEnabled(),
+                u.getEmail(),
+                u.getPassword(),
+                u.isEnabled(),
                 true,
                 true,
                 true,
-                GeneralUtils.buildSimpleGrantedAuthorities(user.getRoles()), user);
+                GeneralUtils.buildSimpleGrantedAuthorities(u.getRoles()), u);
     }
 }
