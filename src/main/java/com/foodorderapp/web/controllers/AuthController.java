585e9204d7f112dd461dac098296938953fed034
VladAngelov <v.angelov1919@gmail.com>
2021-01-09 00:34:58 +0200

    Fixed user loading.

--- src/main/java/com/foodorderapp/web/controllers/AuthController.java
+++ src/main/java/com/foodorderapp/web/controllers/AuthController.java
@@ -10,9 +10,7 @@ import com.foodorderapp.models.binding.user.LocalUserBindingModel;
 import com.foodorderapp.security.jwt.TokenProvider;
 import com.foodorderapp.services.interfaces.UserService;
 import com.foodorderapp.util.GeneralUtils;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +22,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
 
 @Slf4j
@@ -45,7 +42,9 @@ public class AuthController {
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequestBindingModel loginRequest) {
         Authentication authentication =
                 authenticationManager
-                        .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(),
+                        .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                loginRequest.getEmail(),
                                 loginRequest.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
--- src/main/java/com/foodorderapp/web/controllers/UserController.java
+++ src/main/java/com/foodorderapp/web/controllers/UserController.java
@@ -10,49 +10,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping(Links.API)
-//public class UserController {
-//
-
-//
-//    @PostMapping(path = Links.USER_REGISTER)
-//    public ResponseEntity<UserViewModel> Register(
-//            @RequestBody UserAddBindingModel userAddBindingModel) {
-//        try {
-//            UserViewModel user = this.modelMapper
-//                    .map(
-//                        this.userService
-//                                .register(this.userService
-//                                        .register(this.modelMapper
-//                                                .map(userAddBindingModel,
-//                                                        UserServiceModel.class))),
-//                            UserViewModel.class);
-//
-//            return new ResponseEntity<>(user, HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//    }
-//
-//    @GetMapping("/user/{email}")
-//    public ResponseEntity<UserViewModel> GetUser(@PathVariable String email) {
-//        try {
-//            UserViewModel user = this.modelMapper
-//                    .map(
-//                            this.userService.getUser(email),
-//                            UserViewModel.class
-//                    );
-//
-//            return new ResponseEntity<>(user, HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//    }
-//}
-
-
 
 @RestController
 @RequestMapping(Links.API) // @RequestMapping("/api")
@@ -68,33 +25,10 @@ public class UserController {
         this.modelMapper = modelMapper;
     }
 
-
     @GetMapping(path = Links.USER_CURRENT)
-//    @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<?> getCurrentUser(@CurrentUser LocalUserBindingModel user) {
         var userViewModel = this.modelMapper.map(this.userService.findUserByEmail(user.getEmail()), UserViewModel.class);
         return ResponseEntity.ok(GeneralUtils.buildUserInfo(user));
     }
-
-    @GetMapping(path = Links.HOME)
-//    @GetMapping("/all")
-    public ResponseEntity<?> getContent() {
-        return ResponseEntity.ok("Public content goes here");
-    }
-
-    @GetMapping(path = Links.USER)
-//    @GetMapping("/user")
-    @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> getUserContent() {
-        return ResponseEntity.ok("User content goes here");
-    }
-
-    @GetMapping(path = Links.WORKER)
-//    @GetMapping("/admin")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> getAdminContent() {
-        return ResponseEntity.ok("Admin content goes here");
-    }
-
 }
